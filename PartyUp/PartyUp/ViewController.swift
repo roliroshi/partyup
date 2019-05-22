958dd8615236c266cc833535a80343eef7b174c4
Roland Iana <roland.iana@students.fh-hagenberg.at>
2019-05-22 11:41:13 +0200

    init

---
+++ PartyUp/PartyUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyUp
+//
+//  Created by roli on 22.05.19.
+//  Copyright © 2019 roli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
