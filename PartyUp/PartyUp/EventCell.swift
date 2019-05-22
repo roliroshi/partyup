68f0858c1dfa38efa82499f4b9111aa8d220f8b3
Roland Iana <roland.iana@students.fh-hagenberg.at>
2019-05-22 20:00:01 +0200

    TableView and cells added

---
+++ PartyUp/PartyUp/EventCell.swift
@@ -0,0 +1,18 @@
+//
+//  EventCell.swift
+//  PartyUp
+//
+//  Created by roli on 22.05.19.
+//  Copyright © 2019 roli. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
